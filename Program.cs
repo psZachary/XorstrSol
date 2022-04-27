d680b7db2ceae8f6a4cf69eccead161ff3d58b1c
zack <zacklesler@Outlook.com>
2022-04-27 09:09:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace XorstrSol
+{
+    class Program
+    {
+        private static readonly string[] XorstrFileExtensions = { ".cpp", ".h", ".c", ".hpp" };
+
+        static void Main(string[] args)
+        {
+            foreach (string fileExtension in XorstrFileExtensions)
+            {
+                foreach (string file in Directory.EnumerateFiles("..\\", "*" + fileExtension, SearchOption.AllDirectories))
+                {
+                    var result = Xorstr.ParseFile(file);
+
+                    if (!result.Item1)
+                    {
+                        Console.WriteLine(result.Item2.Error);
+                    }
+
+                    Console.WriteLine(result.Item2.StringDataBuffer);
+                }
+            }
+
+        }
+    }
+}
---
+++ Xorstr.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace XorstrSol
+{
+    enum XorstrError
+    {
+        None,
+        InvalidFile
+    }
+    class Xorstr
+    {
+        public byte[] DataBuffer { get; set; }
+
+        public string StringDataBuffer { get; set; }
+
+        public XorstrError Error { get; set; }
+
+        public Xorstr()
+        {
+            StringDataBuffer = "";
+            Error = XorstrError.None;
+        }
+
+        public static Tuple<bool, Xorstr> ParseFile(string filePath)
+        {
+            Xorstr xorstr = new Xorstr();
+
+            byte[] fileData = File.ReadAllBytes(filePath);
+            if (fileData.Length <= 0)
+            {
+                xorstr.Error = XorstrError.InvalidFile;
+                return new Tuple<bool, Xorstr>(false, xorstr);
+            }
+
+            xorstr.DataBuffer = new byte[fileData.Length];
+
+            var fileText = Encoding.UTF8.GetString(fileData);
+            if (fileText == "" || fileText.Length == 0)
+            {
+                xorstr.Error = XorstrError.InvalidFile;
+                return new Tuple<bool, Xorstr>(false, xorstr);
+            }
+
+            bool hasFoundString = false;
+            bool addEndParentheses = false;
+
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                xorstr.StringDataBuffer += fileText[i];
+                if (addEndParentheses)
+                {
+                    xorstr.StringDataBuffer += ")";
+                    addEndParentheses = false;
+                }
+                if (i >= fileText.Length - 1) continue;
+
+                if (fileText[i + 1] == '"' && !hasFoundString)
+                {
+                    hasFoundString = true;
+                    xorstr.StringDataBuffer += "xorstr_(";
+                    continue;
+                }
+                if (fileText[i + 1] == '"' && hasFoundString)
+                {
+                    hasFoundString = false;
+                    addEndParentheses = true;
+                    continue;
+                }
+            }
+
+            return new Tuple<bool, Xorstr>(true, xorstr);
+        }
+    }
+}
